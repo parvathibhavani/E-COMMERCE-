ab59ed77fc985f5d9e656ebf821014ac8fcffc77
Parvathi Periasamy <parvathi@getrapl.com>
2024-01-11 16:44:10 +0530

    TASK: Ecommerce Application

--- PDO/pdodisplayone.php
+++ PDO/pdodisplayone.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document Table</title>
+    <title>Document Table </title>
 </head>
 <body>
     <form method="post" action="">
